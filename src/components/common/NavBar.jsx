21680d99c24d3b00ff0cdbacd400a2d133c95acc
anil-moond <moonda758@gmail.com>
2024-09-04 14:48:07 +0530

    navbar changes

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -4,6 +4,9 @@ const NavBar = () => {
   return (
     <div>
       <p className='bg-red-700 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl'>Navbar</p>
+      <p className='bg-red-700 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl'>Navbar</p>
+      <p className='bg-red-700 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl'>Navbar</p>
+      <p className='bg-red-700 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl'>Navbar</p>
     </div>
   )
 }
